01de5fc0d70eb884e175f4cabc9b84eb7878e103
ZongAng123 <zongang15@yahoo.com>
2019-05-05 14:36:24 +0800

    系统地图Swift版

---
+++ SystemMap-Swift/MyAnnotation.swift
@@ -0,0 +1,15 @@
+//
+//  MyAnnotation.swift
+//  SystemMap-Swift
+//
+//  Created by 纵昂 on 2019/5/5.
+//  Copyright © 2019 纵昂. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class MyAnnotation: NSObject {
+    var coordinate = CLLocationCoordinate2D()
+    var title = NSString()
+    var subtitle = NSString()
+}
